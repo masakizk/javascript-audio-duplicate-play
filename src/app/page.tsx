9a634244e5a525de4663589624c9b20eb5933c83
zacker <robomasaki2@gmail.com>
2023-07-23 10:00:52 +0900

    feat: 音声が二重で再生されない機構を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,90 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+"use client";
+
+import axios from 'axios'
+import { useRef, useState } from 'react';
 
 export default function Home() {
+  const { playMusic, stopMusic } = useMusic();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <button onClick={async () => await playMusic()}>Play Music</button>
+      <button onClick={async () => await stopMusic()}>Stop Music</button>
+    </div>
   )
+}
+
+const useMusic = () => {
+  // 次の音声が再生可能かどうか（再生準備中の音声がないか）
+  const [isReady, setIsReady] = useState(true);
+  const audioSourceRef = useRef<AudioBufferSourceNode | null>(null);
+
+  const playMusic = async () => {
+    console.log({
+      status: "try play",
+      isReady,
+    });
+
+    setIsReady(false);
+    await stopMusic();
+
+    audioSourceRef.current = await new Promise(async (resolve) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const blob = e.target.result as ArrayBuffer;
+        const audioContext = new AudioContext();
+        audioContext.decodeAudioData(blob, (buffer) => {
+          const source = audioContext.createBufferSource();
+
+          source.buffer = buffer;
+          source.connect(audioContext.destination);
+
+          // 再生
+          console.log({
+            status: "play",
+            audioContextState: audioContext.state,
+          })
+          source.start(0);
+          setIsReady(true);
+          resolve(source);
+        });
+      }
+
+      const blob = await fetchAudioBlob();
+      reader.readAsArrayBuffer(blob);
+    });
+  }
+
+  const stopMusic = async () => {
+    await new Promise((resolve) => {
+      console.log({
+        status: "try stop",
+        audioSource: !!audioSourceRef.current,
+      })
+
+      if (audioSourceRef.current) {
+        const audioSource = audioSourceRef.current;
+        audioSource.stop();
+        audioSource.disconnect();
+        audioSourceRef.current = null;
+        console.log({
+          status: "stop",
+        })
+      }
+      
+      resolve(0);
+    });
+  }
+
+  return {
+    playMusic,
+    stopMusic,
+  }
+}
+
+const fetchAudioBlob = async (): Promise<Blob> => {
+  const { data } = await axios.get("/music.mp3", { responseType: 'blob' });
+  return data;
 }
